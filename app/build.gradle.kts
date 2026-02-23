fc80446dd8352462aa1a841a9146dc4ab8b42cb3
AymenMefti <meftiaymen@hotmail.com>
2026-02-22 21:37:42 -0500

    app build update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform()
+        }
+    }
 }
 
 dependencies {
