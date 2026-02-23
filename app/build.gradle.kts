55fa21fd7f4c1fbff767778045fc84c2a3ae8985
AymenMefti <meftiaymen@hotmail.com>
2026-02-22 21:43:41 -0500

    app build update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
 // Unit and component tests (JUnit 5)
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
